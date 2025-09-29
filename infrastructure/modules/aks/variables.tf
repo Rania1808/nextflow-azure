b96a531a4a205e0a7b1066412dcfac2b11d4eef4
Ubuntu <devopsuser@jenkins-server.nzeytzeiahbuhjpcjsa2aza4xe.ax.internal.cloudapp.net>
2025-09-29 13:45:10 +0000

    modules added

---
+++ infrastructure/modules/aks/variables.tf
@@ -0,0 +1,19 @@
+variable "resource_group_name" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "cluster_name" {
+  type = string
+}
+
+variable "node_count" {
+  type = number
+}
+
+variable "acr_id" {
+  type = string
+}
